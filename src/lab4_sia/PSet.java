b4b1b4abbf6060ec72426dcdb5e8a94e84a709a1
Владимир <zlumyo@gmail.com>
2013-05-15 15:36:46 +0400

    Создан внтуренний для PSet класс Node, который представляет собой узел двочиного дерева. Дописаны все документирующие комментарии.

--- src/lab4_sia/PSet.java
+++ src/lab4_sia/PSet.java
@@ -2,35 +2,72 @@
 package lab4_sia;
 
 /**
- *
- * @author Владимир
+ * Контейнер типа "множество".
+ * 
+ * @author      Владимир
+ * @param <T>   тип элементов множества 
  */
-public class PSet<T> {
+public class PSet<T extends Comparable> {
     
     /**
-     *
-     * @param item
-     * @return
+     * Узел двоичного дерева.
+     * @author Владимир
+     */
+    private class Node<T extends Comparable> {
+        public T data;              /** Данные хранящиеся в узле.*/
+        public Node left, right;    /** Дочерние элементы узла.*/
+        
+        /**
+         * Создаёт узел с данными.
+         */
+        public Node(T data) {
+            this.data = data;
+            left = null;
+            right = null;
+        }
+    }
+    
+    private Node root; /** Корень двочиного дерева поиска.*/
+    
+    /**
+     * Создаёт пустое множество.
+     */
+    public PSet() {
+        root = null;
+    }
+    
+    /**
+     * Добавляет новый эелемент в множество.
+     * @param item  добавляемый элемент
+     * @return      true при успешном добавлении, иначе - false
      */
     public boolean add(T item) {
         return true;
     }
     
     /**
-     *
-     * @param item
-     * @return
+     * Удаляет указанный элемент из множества.
+     * @param item  удаляемый элемент
+     * @return      true при успешном удалении, иначе - false
      */
     public boolean remove(T item) {
         return true;
     }
     
     /**
-     *
-     * @param item
-     * @return
+     * Проверяет наличие указанного элемента в множестве.
+     * @param item  проверяемый элемент
+     * @return      true при успешном нахождении, иначе - false
      */
     public boolean contains(T item) {
         return true;
+    }
+    
+    /**
+     * Возвращает мощность множества.
+     * @return мощность множества
+     */
+    public int size() {
+        return 0;
     }
 }
