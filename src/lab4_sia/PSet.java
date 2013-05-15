d71c69f3064da23a48d6dcd57ef35826ac004ef2
Владимир <zlumyo@gmail.com>
2013-05-15 14:55:30 +0400

    Объявлен класс PSet, реализующий контейнер типа "множество".

---
+++ src/lab4_sia/PSet.java
@@ -0,0 +1,36 @@
+
+package lab4_sia;
+
+/**
+ *
+ * @author Владимир
+ */
+public class PSet<T> {
+    
+    /**
+     *
+     * @param item
+     * @return
+     */
+    public boolean add(T item) {
+        return true;
+    }
+    
+    /**
+     *
+     * @param item
+     * @return
+     */
+    public boolean remove(T item) {
+        return true;
+    }
+    
+    /**
+     *
+     * @param item
+     * @return
+     */
+    public boolean contains(T item) {
+        return true;
+    }
+}
